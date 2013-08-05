4df1c04143fbf8b2c4d3e725bfda7124f79d87fa
Siarhei <skhilko@gmail.com>
2013-08-04 22:01:37 -0700

    added placeholders for api methods

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+(function(window, undefined) {
+	'use strict';
+	function Prompt(element) {
+		this.element = element;
+	}
+
+	Prompt.prototype.open = function() {
+		console.log('open');
+	};
+
+	Prompt.prototype.close = function() {
+		console.log('close');
+	};
+
+	window.Prompt = Prompt;
+})(window);
