8790a5cad717e831ca393540815726a33cb84c82
Siarhei Khilko <skhilko@gmail.com>
2013-08-07 22:31:46 -0700

    adding the ability to close the dialog on 'esc' key press

--- src/scripts/prompt.js
+++ src/scripts/prompt.js
@@ -1,17 +1,27 @@
 (function(window, undefined) {
 	'use strict';
 
+	// TODO
+	// -  focus the first focusable element inside of the dialog on open
+	// -  return focus to the dialog when clicked outside
+	// -? remove tabIndex on destroy
+
 	function Prompt(element) {
-		this._handlers = [];
+		this.handlers = [];
 
 		this.element = element;
+		// make the dialog focusable
+		this.element.tabIndex = -1;
+
 		this.closeButton = element.querySelector('.prompt-action-close');
 
-		this._on('click', this.closeButton, _closeHandler);
+		this._on('click', this.closeButton, closeHandler);
+		this._on('keydown', this.element, escCloseHandler);
 	}
 
 	Prompt.prototype.open = function() {
 		this.element.classList.add('prompt-show');
+		this.element.focus();
 	};
 
 	Prompt.prototype.close = function() {
@@ -23,9 +33,11 @@
 		_removeEventHandlers(this);
 	};
 
+	var KEY_CODE_ESCAPE = 27;
+
 	Prompt.prototype._on = function(event, element, handler) {
 		handler = handler.bind(this);
-		this._handlers.push({
+		this.handlers.push({
 			event: event,
 			element: element,
 			handler: handler
@@ -33,18 +45,26 @@
 		element.addEventListener(event, handler, false);
 	};
 
-	function _closeHandler() {
+	function closeHandler() {
 		this.close();
 	}
 
+	function escCloseHandler(event) {
+		if (event.keyCode === KEY_CODE_ESCAPE) {
+			event.preventDefault();
+			this.close();
+			return;
+		}
+	}
+
 	function _removeEventHandlers(instance) {
-		for (var i = 0, len = instance._handlers.length; i < len; i++) {
-			var event = instance._handlers[i].event;
-			var element = instance._handlers[i].element;
-			var handler = instance._handlers[i].handler;
+		for (var i = 0, len = instance.handlers.length; i < len; i++) {
+			var event = instance.handlers[i].event;
+			var element = instance.handlers[i].element;
+			var handler = instance.handlers[i].handler;
 			element.removeEventListener(event, handler);
 		}
-		delete instance._handlers;
+		delete instance.handlers;
 	}
 
 	window.Prompt = Prompt;
